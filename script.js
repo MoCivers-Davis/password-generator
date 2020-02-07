ff9ca94596f3d5a2a97fa046b96140ac9118aef7
Mo Civ <mocivers@comcast.net>
2020-02-07 01:02:28 -0500

    added more comments to JavaScript

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+//Pop-ups (Prompts, Alerts, and Confirms along with if/esle conditions)
 var passwordLength = prompt("How many characters would you like your password to conatain?");//******password must be between 8 and 128 characters***********
 if (passwordLength < 8) { //JavaScript condition. Check to see if password length is less than 8 characters.  
   alert ("Password length must be at least 8 charachters");//If less than 8 characters, inform user "password length must be at least 8 charchters".
@@ -39,15 +41,7 @@ console.log (confirmNum);
 console.log (confirmLower);
 console.log (confirmUpper);
 
-//Condition Statements
-
-//$if (passwordLength < 8) {
-//$alert("Password length must be at least 8 characters and no more than 128");
-//$}
-//Check to see if password length is between less than 8 characters or more than 128.  If less than 8 characters, inform user "password length must be at least 8 charchters and no more than 128 characters"
-//$else if (passwordLength > 128){
- //$ alert("Password length must be at least 8 characters and no more than 128");
-//$}
+//use the Math.random function to randomly select a number between 
 
 //$alert ("Congratulations, Your new password is" + )//*****I need to figure out what goes here to spit out the new password***********
 
