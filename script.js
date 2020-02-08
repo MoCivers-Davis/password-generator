ae2b7933e26c1e5af3e97118679f3eabe4443b2b
Mo Civ <mocivers@comcast.net>
2020-02-07 23:02:33 -0500

    more updates to javascript

--- script.js
+++ script.js
@@ -22,13 +22,14 @@ if (passwordLength < 8) { //JavaScript condition. Check to see if password lengt
 } else if (passwordLength > 128) {//JavaScript condition. //Check to see if password length is more than 128 characters.   
   alert ("Password length must be less than 128 characters");//If more than 128 characters, inform user "password length must not be more than 128 characters".
 }
+
 var confirmSpecial = confirm("Click to confirm including special characters");//A pop will ask if you would like to include special characters
 var confirmNum = confirm("Click to confrim including numeric characters");//A pop-up will ask if you would like to include numeric characters
 var confirmLower = confirm("Click to confirm including lowercase charaters");//A pop-up will ask if you would like to include lowercase characters
 var confirmUpper = confirm("Click to confirm including uppercase charaters");//A pop-up will ask if you would like to include upper case characters
 
 //Declaring Varialbles
-//$var passwordLength =$ //Declaring variable. Saving charater amount
+//passwordLength = //Declaring variable. Saving charater amount
 var confirmSpecial = true//Declaring variable. Saving confirmSpecial variable to true. This is considered a true/false boolean
 var confirmNum = true;//Declaring variable. Saving confirmNum variable to true.  This is considered a true/false boolean
 var confirmLower = true;//Declaring variable. Saving confirm variable to true. This is considered a true/false boolean
@@ -43,17 +44,21 @@ console.log (confirmLower);
 console.log (confirmUpper);
 
 //Creating and Array
-//var confirmLower = [a b c d e f g h i j k l m n o p q r s t u v w x y z];
-//document.getElementById("demo").innerHTML = cars; <= uncomment this section when ready to test array
+var lower = ["a b c d e f g h i j k l m n o p q r s t u v w x y z"];
+document.getElementById("password").innerHTML = lower; 
+console.log (upper)
 
-//var confirmUpper = [A B C D E F G H I J K L M N O P Q R S T U V W X Y Z];
-//document.getElementById("demo").innerHTML = cars; <= uncomment this section when ready to test array
+var upper = ["A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"];
+document.getElementById("password").innerHTML = upper; 
+console.log (upper)
 
-//var confirmSpecial = [! @ # % ^ & * ( ) + { } | : ?];
-//document.getElementById("demo").innerHTML = cars; <= uncomment this section when ready to test array
+var special = ["! @ # % ^ & * ( ) + { } | : ?"];
+document.getElementById("password").innerHTML = special; 
+console.log (special)
 
-//var confirmNum = [0 1 2 3 4 5 6 7 8 9];
-//document.getElementById("demo").innerHTML = cars; <= uncomment this section when ready to test array
+var num = ["0 1 2 3 4 5 6 7 8 9"];
+document.getElementById("password").innerHTML = num; 
+console.log (num)
 
 
 
@@ -61,20 +66,20 @@ console.log (confirmUpper);
 
 //$alert ("Congratulations, Your new password is" + )//*****I need to figure out what goes here to spit out the new password***********
 
-//function getRandomLower () {
+//function getlower () {
  // return String.fromCharCode(Math.floor(Math.random() *26) + 97);
 //}
 
-//function getRandomUpper () {
+//function getupper () {
   //return String.fromCharCode(Math.floor(Math.random() *26) + 65);
 //}
 
-//function getRandomNumber () {
+//function getnumber () {
   //return String.fromCharCode(Math.floor(Math.random() *10) + 48);
 //}
 
-//function getRandomSymbol () {
- // const symbols = "!@#%^&*(){}[]+<>,.";
- // return symbols[(Math.floor(Math.random() * symbols.length)];
+//function getspecial () {
+ // const special = "!@#%^&*(){}[]+<>,.";
+ // return special[(Math.floor(Math.random() * special.length)];
 //}
-//console.log(getRandomUpper());
+//console.log(getspecial());
