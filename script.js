0ca21f82831b43f2a97abbb97b813d8458de87c2
Mo Civ <mocivers@comcast.net>
2020-02-12 20:28:19 -0500

    updated js code

--- script.js
+++ script.js
@@ -47,23 +47,23 @@ passwordOptions()
 //Creating Arrays
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", ",l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 document.getElementById("password").innerHTML = lower;
-console.log(upper)
+//console.log(upper)
 
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 document.getElementById("password").innerHTML = upper;
-console.log(upper)
+//console.log(upper)
 
 var special = ["!", "@", "#", "%", "^", "&", "*", "(", ")", "+", "{", "}", "|", ":", "?"];
 document.getElementById("password").innerHTML = special;
-console.log(special)
+//console.log(special)
 
 var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 document.getElementById("password").innerHTML = num;
-console.log(num)
+//console.log(num)
 
-var children = lower.contact(upper, special, num);
-document.getElementById("password").innerHTML = children;
-console.log(children)
+//var children = lower.contact(upper, special, num);
+//document.getElementById("password").innerHTML = children;
+//console.log(children)
 
 //var newArray = []
 //var i;
