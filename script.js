1ce5454e68ae9b1786b492939c27cec76f4609cd
Mo Civ <mocivers@comcast.net>
2020-02-12 20:09:46 -0500

    updated javaScript and readme

--- script.js
+++ script.js
@@ -7,64 +7,73 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 console.log("generate password");
 
+function passwordOptions() {
+  //Pop-ups (Prompts, Alerts, and Confirms along with if/esle conditions)
+  var passwordLength = prompt("How many characters would you like your password to contain?");//******password must be between 8 and 128 characters***********
+  if (passwordLength < 8) { //JavaScript condition. Check to see if password length is less than 8 characters.  
+    alert("Password length must be at least 8 charachters");//If less than 8 characters, inform user "password length must be at least 8 charchters".
+    return;
+  } else if (passwordLength > 128) {//JavaScript condition. //Check to see if password length is more than 128 characters.   
+    alert("Password length must be less than 128 characters");//If more than 128 characters, inform user "password length must not be more than 128 characters".
+    return;
+  }
 
-//Pop-ups (Prompts, Alerts, and Confirms along with if/esle conditions)
-var passwordLength = prompt("How many characters would you like your password to contain?");//******password must be between 8 and 128 characters***********
-if (passwordLength < 8) { //JavaScript condition. Check to see if password length is less than 8 characters.  
-  alert ("Password length must be at least 8 charachters");//If less than 8 characters, inform user "password length must be at least 8 charchters".
-} else if (passwordLength > 128) {//JavaScript condition. //Check to see if password length is more than 128 characters.   
-  alert ("Password length must be less than 128 characters");//If more than 128 characters, inform user "password length must not be more than 128 characters".
+  var confirmSpecial = confirm("Click to confirm including special characters");//A pop will ask if you would like to include special characters
+  var confirmNum = confirm("Click to confrim including numeric characters");//A pop-up will ask if you would like to include numeric characters
+  var confirmLower = confirm("Click to confirm including lowercase charaters");//A pop-up will ask if you would like to include lowercase characters
+  var confirmUpper = confirm("Click to confirm including uppercase charaters");//A pop-up will ask if you would like to include upper case characters
+
+  if (confirmLower == true) {
+    newArray.push(lower)
+  }
+  if (confirmUpper == true) {
+    newArray.push(upper)
+  }
+  if (confirmNum == true) {
+    newArray.push(num)
+  }
+  if (confirmSpecial == true) {
+    newArray.push(special)
+  }
 }
+passwordOptions()
 
-var confirmSpecial = confirm("Click to confirm including special characters");//A pop will ask if you would like to include special characters
-var confirmNum = confirm("Click to confrim including numeric characters");//A pop-up will ask if you would like to include numeric characters
-var confirmLower = confirm("Click to confirm including lowercase charaters");//A pop-up will ask if you would like to include lowercase characters
-var confirmUpper = confirm("Click to confirm including uppercase charaters");//A pop-up will ask if you would like to include upper case characters
+//Creating Arrays
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", ",l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+document.getElementById("password").innerHTML = lower;
+console.log(upper)
 
-//Declaring Varialbles
-//passwordLength = //Declaring variable. Saving charater amount
-var confirmSpecial = true//Declaring variable. Saving confirmSpecial variable to true. This is considered a true/false boolean
-var confirmNum = true;//Declaring variable. Saving confirmNum variable to true.  This is considered a true/false boolean
-var confirmLower = true;//Declaring variable. Saving confirm variable to true. This is considered a true/false boolean
-var confirmUpper = true;//Declaring variable. Saving confirm variable to true. This is considered a true/false boolean
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+document.getElementById("password").innerHTML = upper;
+console.log(upper)
 
+var special = ["!", "@", "#", "%", "^", "&", "*", "(", ")", "+", "{", "}", "|", ":", "?"];
+document.getElementById("password").innerHTML = special;
+console.log(special)
 
-//Utilized Console.log feature in Chrome Developers Tools to debug varialbles  
-//$console.log (passwordLength);
-console.log (confirmSpecial);
-console.log (confirmNum);
-console.log (confirmLower);
-console.log (confirmUpper);
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+document.getElementById("password").innerHTML = num;
+console.log(num)
 
+var children = lower.contact(upper, special, num);
+document.getElementById("password").innerHTML = children;
+console.log(children)
 
-var lower = ["a b c d e f g h i j k l m n o p q r s t u v w x y z"];
-document.getElementById("password").innerHTML = lower; 
-console.log (upper)
+//var newArray = []
+//var i;
+//for (i = 0; i < newArray.length; i++) {
+  //text += cars[i] + "<br>";
+//}
+//create a for loop to go through new array 
+//upperArray
 
-var upper = ["A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"];
-document.getElementById("password").innerHTML = upper; 
-console.log (upper)
-
-var special = ["! @ # % ^ & * ( ) + { } | : ?"];
-document.getElementById("password").innerHTML = special; 
-console.log (special)
-
-var num = ["0 1 2 3 4 5 6 7 8 9"];
-document.getElementById("password").innerHTML = num; 
-console.log (num)
-
-var children = lower.contact (upper, special, num);
-document.getElementById("password").innerHTML = children; 
-console.log (children)
+//lowerArray
 
 //use the Math.random function to randomly select a number between 
 
